4c6779d429dbb11d87c871d80c99cf72714c3bcc
김하경 <hagyoung0521@naver.com>
2021-07-15 18:58:18 +0900

    slide, headerScroll, innerHTML추가 등 변경

---
+++ public/index2.js
@@ -0,0 +1,118 @@
+
+
+
+
+
+//변수 지정
+// var slideWrap = document.querySelector(".container2");    // 여기서 container2가 여러개이고 각자의 역할이 있다면 querySelectorAll을 써준다.
+// var slideContainer = document.querySelector(".slider2");
+// var slide = document.querySelectorAll(".item2");     //li는 여러개이고 각각 값이 다르기 때문에 querySelectorAll 을 사용 (all은 배열을 의미) 
+// var nextBtn2 = document.querySelector('.next2');
+// var prevBtn2 = document.querySelector('.prev2');
+// // var slideWidth = 0;
+// var totalslide = slide.length/3; // li갯수
+// var currentIndex = 0;
+
+// var nextBtn = document.getElementsByClassName('.next2');
+// var prevBtn = document.getElementsByClassName('.prev2');  처럼 id, class명에 맞춰서 사용해도 무관.
+
+//슬라이드 높이의 기본값은 0
+
+
+
+//슬라이드 높이 확인하여 부모의 높이로 지정하기   대상.offsetHeight : 대상의 높이을 확인
+//$slideHeight = $slide[0].offsetHeight;  > slide의 첫번째의 높이를 가져와서 slideHeight에 담음
+
+// for(var i = 0; i < totalslide; i++){
+//     if(slideWidth < slide[i].offsetWidth){
+//         slideWidth = slide[i].offsetWidth;
+// //     }
+// // }
+
+// $slideWrap.style.width = slideWidth + 'px';
+// $slideContainer.style.width = slideWidth + 'px';
+
+// // var slideWidth = $slideWrap.clientWidth;  // margin값과 border값이 제외된, padding값까지만 적용된 container2의 width
+// // slide.style.width = slideWidth * totalslide + 'px'; // container2길이 * li 갯수(한번에 3개씩 보여줄 것이기 때문에 /3)
+
+
+// //슬라이드 가로 배열
+// for(var a = 0; a < totalslide; a++){
+//     slide[a].style.left = a * 100 + '%';
+// }
+
+// function goToSlide(idx){
+//     $slideContainer.style.left = -100 * idx + '%';
+//     currentIndex = idx;
+// }
+
+// nextBtn2.addEventListener('click', function(){
+//     // goToSlide(currentIndex + 1);
+
+//     if(currentIndex == totalslide - 1){
+//         goToSlide(0);
+//     }else{
+//         goToSlide(currentIndex + 1);
+//     }
+// });
+
+// prevBtn2.addEventListener('click', function(){
+//     // goToSlide(currentIndex - 1);
+
+//     if(currentIndex == 0){
+//         goToSlide(totalslide - 1);
+//     }else{
+//         goToSlide(currentIndex - 1);
+//     }
+// });
+
+
+
+
+
+
+
+var slideWrapper = document.querySelector('.container2');
+var slideUl = document.querySelector('.slider2');
+var slides = document.querySelectorAll('.item2');
+var nextBtn = document.querySelector('.next2');
+var prevBtn = document.querySelector('.prev2');
+var totalSlides = slides.length/3; // item의 갯수/3
+
+
+
+var sliderWidth = slideWrapper.clientWidth; // container의 width
+var slideIndex = 0;
+
+
+slideUl.style.width = sliderWidth * totalSlides + 'px';
+
+
+function showSlides(n) {
+    slideIndex = n;
+    if (slideIndex == -1) {   //li의 번호가 -1이라면
+        slideIndex = totalSlides - 1;  //li의 갯수에서 -1을 해라
+    } else if (slideIndex === totalSlides) {  //li의 번호가 li의 갯수와 같다면(마지막li이라면)
+        slideIndex = 0;   //li의 0번으로 가라(첫번째li로)
+    }
+    slideUl.style.left = -(sliderWidth * slideIndex) + 'px'; //왼쪽으로 이동
+    
+    console.log(slideIndex);
+}
+
+function plusSlides(n){
+    showSlides(slideIndex += n);
+}
+  
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+nextBtn.addEventListener('click', function () {
+    plusSlides(1);
+});
+prevBtn.addEventListener('click', function () {
+    plusSlides(-1);
+});
+
+
--- public/script.js
+++ public/script.js
@@ -7,16 +7,53 @@ function dropdown_menu() {
 window.click = function (event) {
     if (!event.target.matches('.dropbtn')) {
 
-        var language_dropdown = document.getElementsByClassName("language-content");
-        var i;
+        const language_dropdown = document.getElementsByClassName("language-content");
+        let i;
         for (i = 0; i < language_dropdown; i++) {
-            var openDropdown = language_dropdown[i];
+            const openDropdown = language_dropdown[i];
+
+
+            
             if (openDropdown.classList.contains('show')) {
                 openDropdown.classList.remove('show')
             }
         }
     }
 }
+
+
+//헤더 스크롤
+    const contents = document.querySelector("#main");
+    const h = document.querySelector("header");
+    let Header_height ;
+  
+    function seTopPadding(){
+     Header_height = h.offsetHeight;  //header의 높이값을 가져옴
+     content.style.paddingTop = Header_height + "px";
+    }
+  
+    function onScroll(){
+      window.addEventListener("scroll", callbackFnc);
+      function callbackFnc(){
+        const y = window.pageYOffset;  //수직으로 스크롤 된 만큼의 픽셀값을 계산(pageYOffset)
+
+
+        y > 300 ? h.classList.add("scroll") : h.classList.remove("scroll");
+        // if(y > 300){
+        //   h.classList.add("scroll");
+        // } else {
+        //   h.classList.remove("scroll");
+        // }
+      }
+    }
+  
+    window.onload = function(){
+      onScroll();
+      seTopPadding();
+    };
+  
+ 
+
 
 
 //input 경고창
@@ -28,45 +65,95 @@ window.click = function (event) {
 
 
 
+//슬라이드 메뉴 
+document.querySelector(".content_manu1").addEventListener('click', function(){
+    //https://anko3899.tistory.com/218 해보기
+    document.querySelector(".content-under-txt").innerHTML = "바른뉴스";
+
+});
+document.querySelector(".content_manu2").addEventListener('click', function(){
+    document.querySelector(".content-under-txt").innerHTML = "업무사례";
+});
+document.querySelector(".content_manu3").addEventListener('click', function(){
+    document.querySelector(".content-under-txt").innerHTML = "언론보도";
+});
+
+
+
+
+
+
+
+
+
 //슬라이드
-var slideWrapper = document.querySelector('.list-wrapper-slider');
-var slides = document.querySelectorAll('.list-wrapper-slider ul li');
-var totalSlides = slides.length/3; // li 갯수
+const slideWrapper = document.querySelector('.list-wrapper-slider');
+const slider = document.querySelector('.list-wrapper-slider ul');
+const slides = document.querySelectorAll('.list-wrapper-slider ul li');
+const totalSlides = slides.length/3;
+
+const nextBtn = document.querySelector('.next');
+const prevBtn = document.querySelector('.prev');
+
+
 
 console.log('slideWrapper',slideWrapper)
-var sliderWidth = slideWrapper.clientWidth; // list-wrapper-slider width
-var slideIndex = 0;
-var slider = document.querySelector('.list-wrapper-slider ul');
+const sliderWidth = slideWrapper.clientWidth; // list-wrapper-slider width
+let slideIndex = 0;
+
 
 slider.style.width = sliderWidth * totalSlides + 'px';
 
 
 function showSlides(n) {
     slideIndex = n;
-    if (slideIndex == -1) {
-        slideIndex = totalSlides - 1;
-    } else if (slideIndex === totalSlides) {
-        slideIndex = 0;
-    }
+
+    (slideIndex == -1) ?  slideIndex = totalSlides - 1 : (slideIndex === totalSlides) ? slideIndex = 0 : slideIndex = n;
+    // if (slideIndex == -1) {
+    //     slideIndex = totalSlides - 1;
+    // } else if (slideIndex === totalSlides) {
+    //     slideIndex = 0;
+    // } //조건부연산자로 변경
+
     slider.style.left = -(sliderWidth * slideIndex) + 'px';
-    console.log(slideIndex);
+    // console.log(slideIndex);
 }
+
+
 
 function plusSlides(n){
-    showSlides(slideIndex += n);
+    showSlides(slideIndex += n);  //next버튼을 클릭했을 때 현재 인덱스번호 +1을 대입한 li를 보여준다
+    console.log(slideIndex);
+
+    slideIndex == totalSlides - 1 ? nextBtn.classList.add('disabled') :  nextBtn.classList.remove('disabled');
+    slideIndex > 0 ?  prevBtn.classList.add('disabled') :  prevBtn.classList.remove('disabled');
+    // if(slideIndex == totalSlides - 1){
+    //     nextBtn.classList.add('disabled');
+    // }else{
+    //     nextBtn.classList.remove('disabled');
+    // }
 }
   
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
+// function currentSlide(n) {
+//     showSlides(slideIndex = n);
+//     console.log(slideIndex);
+//     // slideIndex > 0 ?  prevBtn.classList.add('disabled') :  prevBtn.classList.remove('disabled');
 
-var nextBtn = document.querySelector('.next');
-var prevBtn = document.querySelector('.prev');
+// }
+
 
 nextBtn.addEventListener('click', function () {
-    // console.log("123123")
     plusSlides(1);
 });
+
 prevBtn.addEventListener('click', function () {
     plusSlides(-1);
 });
+
+
+
+
+
+
+
+
