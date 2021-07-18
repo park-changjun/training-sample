82d037e6382b9e342609b2bfe0cdbe9ae441f600
BO-LIKE-CHICKEN <78058734+BO-LIKE-CHICKEN@users.noreply.github.com>
2021-07-18 13:34:52 +0900

    두번째 과제 html 및 css 수, header 배경변경 추가

--- public/project3/project3.js
+++ public/project3/project3.js
@@ -9,28 +9,34 @@ const submitPolicy = () => prompt("어떤 정책을 제안하고 싶으신가요
 let todayWeather;
 
 // 날짜 정보를 불러오는 함수
-async function getWeatherInfo(){
-    try{
+async function getWeatherInfo() {
+    try {
         const response = await axios.get(weatherUrl);
         console.log(response);
         return response;
-    } catch(error){
+    } catch (error) {
         console.error(error);
-    } 
+    }
 }
 
 // 오늘의 날짜를 팝업창으로 보여주는 함수
-async function showTodayWeather(){
+async function showTodayWeather() {
     const weatherInfo = await getWeatherInfo();
     const todayWeather = weatherInfo.data.daily[0].weather[0].description;
     alert(`오늘 서울의 날씨는 ${todayWeather}입니다.`);
 }
 
 // 메인 버튼에 클릭 이벤트 추가
-mainBtn.addEventListener('click',submitPolicy);
+mainBtn.addEventListener('click', submitPolicy);
 
 // 고정 버튼에 클릭 이벤트 추가
-fixedBtn.addEventListener('click',submitPolicy);
+fixedBtn.addEventListener('click', submitPolicy);
 
 // 메인 로고에 클릭 이벤트 추가
 logo.addEventListener('click', showTodayWeather);
+
+const header = document.querySelector('header');
+const changeHeaderBg = () => {
+    header.style.backgroundImage = window.scrollY >= 970 ? 'url(img/pattern.png)' : 'unset';
+}
+document.addEventListener('scroll', changeHeaderBg);
