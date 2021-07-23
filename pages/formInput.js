1760ba5bcb1f055ad28d245ed131ec3c3fa1ba15
백보성 <teabs1125@gmail.com>
2021-07-23 18:55:56 +0900

    비밀번호와 비밀번호 확인이 일치해야 회원가입 버튼 활성화

--- pages/formInput.js
+++ pages/formInput.js
@@ -1,27 +1,53 @@
+import axios from 'axios';
 import React from 'react';
+const API_URL = 'http://localhost:8100/api/posts';
 
-const FormInput = ({text, btnText, setForm, info}) => {
-    if(btnText){
+const FormInput = ({ text, btnText, setForm, info, registerForm, setIsConfirm, setIsEqual }) => {
+    const emailOk = () => {
+        alert('사용할 수 있는 이메일입니다!');
+        setIsConfirm(true);
+    }
+    if (btnText) {
         return (
             <div className="form-input-component">
-                <input placeholder={text} onChange={(e) => { 
+                <input placeholder={text} onChange={(e) => {
                     setForm((prev) => (
-                        { ...prev, [info]: e.target.value 
-                        })) }}/>
-                <button >{btnText}</button>
+                        {
+                            ...prev, [info]: e.target.value
+                        }))
+                }} />
+                <button onClick={() => {
+                    axios({ url: API_URL, method: 'GET' })
+                        .then(({ data }) => {
+                            data.filter(member => member.email === registerForm.email).length
+                                ? alert('사용할 수 없는 이메일입니다. 다른 이메일을 사용해주세요.')
+                                : emailOk()
+                        }
+                        )
+                }
+                } >{btnText}</button>
             </div>
         )
-    }else{
+    } else {
         return (
             <div className="form-input-component">
-                <input placeholder={text} onChange={(e) => { 
-                    setForm((prev) => (
-                        { ...prev, [info]: e.target.value 
-                        })) }}/>
+                {setIsEqual
+                    ? <input placeholder={text} onChange={(e) => {
+                        e.target.value === registerForm.password ? setIsEqual(true) : setIsEqual(false)
+                    }} /> :
+                    <input placeholder={text} onChange={(e) => {
+                        setForm((prev) => (
+                            {
+                                ...prev, [info]: e.target.value
+                            }))
+                    }} />
+
+                }
+
             </div>
         )
     }
-    
+
 };
 
 export default FormInput;
--- pages/registerMain.js
+++ pages/registerMain.js
@@ -6,36 +6,38 @@ import FormInputLabel from './formInputLabel';
 import { useState, useEffect } from 'react';
 
 
-const RegisterMain = ({isLogin}) => {
-    const [registerForm, setRegisterForm] = useState({ name: '', email: '', password: '', phoneNum: '', terms:false, personal: false });
+const RegisterMain = ({ isLogin }) => {
+    const [registerForm, setRegisterForm] = useState({ name: '', email: '', password: '', phoneNum: '', terms: false, personal: false });
+    const [isConfirm, setIsConfirm] = useState(false);
+    const [isEqual, setIsEqual] = useState(false);
     const [isActive, setIsActive] = useState(false);
-    
+
     useEffect(() => {
-        registerForm.name&&registerForm.email&&registerForm.password&&registerForm.phoneNum&&registerForm.terms&&registerForm.personal?
-        setIsActive(true):
-        setIsActive(false);
+        registerForm.name && registerForm.email && registerForm.password && registerForm.phoneNum && registerForm.terms && registerForm.personal && isConfirm && isEqual ?
+            setIsActive(true) :
+            setIsActive(false);
     }, [registerForm]);
-   
+
     return (
         <main className="register-main">
             <div className="container">
                 <div className="register-wrap">
                     <div className="main-title">간편 회원가입</div>
                     <FormInputLabel text={'이름'} />
-                    <FormInput text={'이름을 입력하세요.'} btnText={false} setForm={setRegisterForm} info={'name'}/>
+                    <FormInput text={'이름을 입력하세요.'} btnText={false} setForm={setRegisterForm} info={'name'} />
                     <FormInputLabel text={'이메일'} />
-                    <FormInput text={'이메일을 입력하세요.'} btnText={'중복확인'} setForm={setRegisterForm} info={'email'}/>
+                    <FormInput text={'이메일을 입력하세요.'} btnText={'중복확인'} setForm={setRegisterForm} info={'email'} registerForm={registerForm} setIsConfirm={setIsConfirm} />
                     <FormInputLabel text={'비밀번호'} />
-                    <FormInput text={'비밀번호를 입력하세요. (8~20자 이내의 영문과 숫자 조합)'} btnText={false} setForm={setRegisterForm} info={'password'}/>
-                    <FormInput text={'비밀번호를 확인해주세요.'} btnText={false}  setForm={setRegisterForm} info={'password'}/>
+                    <FormInput text={'비밀번호를 입력하세요. (8~20자 이내의 영문과 숫자 조합)'} btnText={false} setForm={setRegisterForm} info={'password'} />
+                    <FormInput text={'비밀번호를 확인해주세요.'} btnText={false} setForm={setRegisterForm} info={'password'} registerForm={registerForm} setIsEqual={setIsEqual} />
                     <FormInputLabel text={'휴대폰 번호'} />
-                    <FormInput text={'휴대폰번호를 입력하세요. (숫자만 입력)'} btnText={false} setForm={setRegisterForm} info={'phoneNum'}/>
+                    <FormInput text={'휴대폰번호를 입력하세요. (숫자만 입력)'} btnText={false} info={'phoneNum'} setForm={setRegisterForm} />
                     <div className="register-checklist">
-                        <CheckInput text={'[필수] 이용약관 동의'} detail={'보기'} setForm={setRegisterForm} info={'terms'}/>
-                        <CheckInput text={'[필수] 개인정보수집 및 이용 동의'} detail={'보기'} setForm={setRegisterForm} info={'personal'}/>
+                        <CheckInput text={'[필수] 이용약관 동의'} detail={'보기'} setForm={setRegisterForm} info={'terms'} />
+                        <CheckInput text={'[필수] 개인정보수집 및 이용 동의'} detail={'보기'} setForm={setRegisterForm} info={'personal'} />
                         <CheckInput text={'[선택] 이벤트 및 정보 수신 동의'} />
                     </div>
-                    <BigConfirmButton text={'가입하기'} registerForm={registerForm} isActive={isActive}/>
+                    <BigConfirmButton text={'가입하기'} registerForm={registerForm} isActive={isActive} />
                 </div>
                 <div className="notice-wrap">
                     <div className="notice">공지사항</div>
